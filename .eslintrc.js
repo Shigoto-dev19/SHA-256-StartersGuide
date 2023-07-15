efd7eeb2dbfb6fa28d43968ffe22a0c33974ca02
CreepyK19 <kaffelmahmoud@gmail.com>
2023-07-15 18:11:13 +0300

    added linter

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  extends: [
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier
+    "prettier", // Uses eslint-config-prettier to disable ESLint rules that would conflict with prettier
+  ],
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
